ec314d01b245fccdc7132ce37e1cb793add18f56
jelli0t <jelli0t@users.noreply.github.com>
2017-11-02 11:33:58 +0100

    Update PublicationPostFactory.java

--- src/main/java/org/sobakaisti/mvt/service/PublicationPostFactory.java
+++ src/main/java/org/sobakaisti/mvt/service/PublicationPostFactory.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Qualifier("publicationPostFactory")
 public class PublicationPostFactory extends PostFactory {
 	
 	@Autowired
