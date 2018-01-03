3f5550ca4efb4d17cd6dfb139c5b2e5409f13ef8
jelli0t <jelli0t@hal9000>
2018-01-03 22:57:39 +0100

    article adapt

--- src/main/java/org/sobakaisti/mvt/models/Article.java
+++ src/main/java/org/sobakaisti/mvt/models/Article.java
@@ -1,68 +1,25 @@
 package org.sobakaisti.mvt.models;
 
-import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.Transient;
-import javax.validation.constraints.Size;
-
-import org.sobakaisti.util.CalendarUtil;
 
 @Entity
 @Table(name="article")
-public class Article {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name="id")
-	private int id;
-	
-	@Size(min=2, max=120, message="{validation.warn.title.size}")
-	@Column(name="title")
-	private String title;
+public class Article extends Post {	
 	
 	@Lob
 	@Column(name="content")
 	private String content;
-	@Column(name="slug")
-	private String slug;
-	
-	@Column(name="post_date")
-	@Temporal(TemporalType.TIMESTAMP)
-	private Calendar postDate;
-	
-	@Column(name="lang")
-	private String lang;
-	
-	@Column(name="active")
-	private int active;
-	
-	@ManyToOne(fetch = FetchType.EAGER, cascade =
-        {
-                CascadeType.DETACH,
-                CascadeType.MERGE,
-                CascadeType.REFRESH,
-                CascadeType.PERSIST
-        },
-        targetEntity = Author.class)
-	private Author author;
-	
+		
 	@ManyToMany(fetch = FetchType.EAGER, cascade =
         {
                 CascadeType.DETACH,
@@ -86,59 +43,15 @@ public class Article {
 	@JoinTable(name="article_category", 
 				joinColumns={@JoinColumn(name="article_id")}, 
 				inverseJoinColumns={@JoinColumn(name="category_id")})
-	private List<Category> categories;
-		
-	@Transient
-	private CalendarUtil postDateMeta;
+	private List<Category> categories;		
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}	
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
 	public String getContent() {
 		return content;
 	}
 	public void setContent(String content) {
 		this.content = content;
 	}
-	public String getSlug() {
-		return slug;
-	}
-	public void setSlug(String slug) {
-		this.slug = slug;
-	}
-	public String getLang() {
-		return lang;
-	}
-	public void setLang(String lang) {
-		this.lang = lang;
-	}
-	public Calendar getPostDate() {
-		return postDate;
-	}
-	public void setPostDate(Calendar postDate) {
-		this.postDate = postDate;
-	}	
-	public int getActive() {
-		return active;
-	}
-	public void setActive(int active) {
-		this.active = active;
-	}
-	public Author getAuthor() {
-		return author;
-	}
-	public void setAuthor(Author author) {
-		this.author = author;
-	}
+
 	public List<Tag> getTags() {
 		return tags;
 	}
@@ -153,11 +66,11 @@ public class Article {
 	}
 	
 	/* meta podaci za postDate */
-	public CalendarUtil getPostDateMeta() {
-		if(this.postDate != null) {
-			return new CalendarUtil(this.postDate);
-		} else {
-			return new CalendarUtil();
-		}
-	}	
+//	public CalendarUtil getPostDateMeta() {
+//		if(this.postDate != null) {
+//			return new CalendarUtil(this.postDate);
+//		} else {
+//			return new CalendarUtil();
+//		}
+//	}	
 }
