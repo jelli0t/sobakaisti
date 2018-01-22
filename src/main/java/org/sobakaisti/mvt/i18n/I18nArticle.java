8a99cb389dd88dc1bc954c485fc2d4275e04ac39
jelli0t <jelli0t@archlinux>
2018-01-22 23:18:59 +0100

    translation

---
+++ src/main/java/org/sobakaisti/mvt/i18n/I18nArticle.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package org.sobakaisti.mvt.i18n;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.sobakaisti.mvt.models.Article;
+
+/**
+ * @author jelli0t
+ *
+ */
+@Entity
+@Table(name="i18n_article")
+public class I18nArticle {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id")
+	private int id;
+	
+	@Size(min=2, max=120, message="{validation.warn.title.size}")
+	@Column(name="title")
+	private String title;
+	
+	@Lob
+	@Column(name="content")
+	private String content;
+	
+	@Column(name="lang")
+	private String lang;
+	
+	@ManyToOne(fetch = FetchType.EAGER, cascade =
+        {
+                CascadeType.DETACH,
+                CascadeType.MERGE,
+                CascadeType.REFRESH,
+                CascadeType.PERSIST
+        },
+        targetEntity = Article.class)
+	@JoinColumn(name="article_id")
+	private Article article;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getLang() {
+		return lang;
+	}
+
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+
+	public Article getArticle() {
+		return article;
+	}
+
+	public void setArticle(Article article) {
+		this.article = article;
+	}
+	
+}
