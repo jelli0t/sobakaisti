4042ed99f9f30bd970774aee337f66ef2c1b5024
jelli0t <jelli0t@users.noreply.github.com>
2018-03-14 09:34:23 +0100

    Update TextUtil.java

--- src/main/java/org/sobakaisti/util/TextUtil.java
+++ src/main/java/org/sobakaisti/util/TextUtil.java
@@ -1,12 +1,17 @@
 package org.sobakaisti.util;
 
 public final class TextUtil {
+	/**
+	* Default private constrictor prevents class to be instantiated
+	* */
+	private TextUtil() {}
 	
 	/**
-	 * Check if string is not empty
+	 * Check if string is not empty. 
+	 * Check if input string is not null and checks length of input
 	 * */
-	public static boolean notEmpty(String string) {	
-		return (string != null && string.length() > 0);		
+	public static boolean notEmpty(String input) {	
+		return input != null && input.length() > 0;		
 	}
 	
 	/**
