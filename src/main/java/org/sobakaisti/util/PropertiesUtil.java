40c4f5ffc42dd0e0c431c2cc9a88bf93887ba4a4
jelli0t <jelli0t@users.noreply.github.com>
2018-03-23 16:22:17 +0100

    Update PropertiesUtil.java

--- src/main/java/org/sobakaisti/util/PropertiesUtil.java
+++ src/main/java/org/sobakaisti/util/PropertiesUtil.java
@@ -1,19 +1,33 @@
 package org.sobakaisti.util;
 
 public final class PropertiesUtil {
-  
-  private static final String QUOTES_PROPERTIES_PREFIX = "quotes_";
+	
+	private static final String QUOTES_PROPERTIES_PREFIX = "quotes_";
+  	public static PropertiesUtil quotes = new PropertiesUtil(QUOTES_PROPERTIES_PREFIX);
+	
+	private Properties properties;
+	private static String fsep = System.getProperty("file.separator");
 
-  public static PropertiesUtil quotesProperties = new PropertiesUtil(QUOTES_PROPERTIES_PREFIX);
-
-  private PropertiesUtil(String propreties_prefix) {
-    properties = new Properties();
-    String fileName = propreties_prefix + ".properties";
+  	private PropertiesUtil(String propreties_prefix) {
+		properties = new Properties();
+		// TODO postavi u zaseban dir
+		String fileName = propreties_prefix + getLocaleLanguage() + ".properties";
 		try {			
 			properties.load(new InputStreamReader(PropertiesUtil.class.getClassLoader().getResourceAsStream(fileName), "UTF-8"));
 			log.info("variables.properties file loaded successfully");
 		} catch (Exception e) {
 			log.error(fileName + " file not found: ", e);
 		}
-  }
+ 	}
+	
+	public Properties getProperties() {
+		return properties;
+	}
+	
+	
+	
+	public String getLocaleLanguage() {
+		Locale locale = LocaleContextHolder.getLocale();		
+		return locale != null ? locale.getLanguage() : StringUtil.DEFAULT_LANG_CODE;
+	}
 }
