6c1208a9b0bd7a12565ea352956661de69fb56c2
jelli0t <jelli0t@users.noreply.github.com>
2018-06-12 11:28:31 +0200

    Create MailMessage.java

---
+++ src/main/java/org/sobakaisti/util/MailMessage.java
@@ -0,0 +1,31 @@
+package org.sobakaisti.util;
+
+/**
+ * @author jelli0t
+ * Reprezentuje pomocni objekta za prihvatanje poruke sa kontakt forme 
+ * i slanje tih podataka na e-mail.
+ * 
+ */
+public class MailMessage {
+  /**
+  * E-mail adresa primaoca poruke
+  */
+  private String to;
+  /**
+  * Ime posiljaoca
+  */
+  private String fromName;
+  /**
+  * E-mail adresa posiljaoca poruke
+  */
+  private String fromMail;
+  /**
+  * Naslov E-mail poruke
+  */
+  private String subject;
+  /**
+  * Sadrzaj E-mail poruke
+  */
+  private String message;
+  
+}
