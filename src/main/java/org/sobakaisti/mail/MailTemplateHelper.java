152cd56ad4276ebfb0790afb143feba7d6274e15
jelli0t <jelli0t@users.noreply.github.com>
2018-06-18 13:46:10 +0200

    Update MailTemplateHelper.java

--- src/main/java/org/sobakaisti/mail/MailTemplateHelper.java
+++ src/main/java/org/sobakaisti/mail/MailTemplateHelper.java
@@ -8,7 +8,7 @@ public class MailTemplateHelper {
   
   protected static final Map<String, MailMessage> CONTACT_MESSAGE_VAL_MAP = new HashMap<>(1) {
     {
-      put(CONTACT_MESSAGE_VAL_NAME, new MailMessage());
+      put(CONTACT_MESSAGE_VAL_NAME, null);
     }
   };
   
@@ -19,17 +19,17 @@ public class MailTemplateHelper {
     /**
      * Template poruke sa kontakt forme
      * */
-    CONTACT_FORM_MAIL(CONTACT_FORM_MAIL_TEMP_NAME, false, CONTACT_MESSAGE_VAL_LIST);
+    CONTACT_FORM_MAIL(CONTACT_FORM_MAIL_TEMP_NAME, false, CONTACT_MESSAGE_VAL_MAP);
 
     private String name;
     private boolean html;
-    private List<String> messageValNames;
- //   private Map<String, Object> messageValMap;
+ //   private List<String> messageValNames;
+    private Map<String, Object> messageValMap;
 
-    private MailTemplate(String name, boolean html, List<String> messageValNames) {
+    private MailTemplate(String name, boolean html, Map<String, Object> messageValMap) {
       this.name = name;
       this.html = html;
-      this.messageValNames = messageValNames;
+      this.messageValMap = messageValMap;
     }
 
     public String getName() {
@@ -40,17 +40,17 @@ public class MailTemplateHelper {
       return html;
     }    
 
-    public String getMessageValNames() {
-      return messageValNames;
+    public Map<String, Object> getMessageValMap) {
+      return messageValMap;
     }
     
      public static MailTemplate findTemplateByName(String name){
-	        for(MailTemplate templ : MailTemplate.values()){
-	            if( templ.getName().equals(name))
-	                return templ;
-	        }
-	        return null;
-	    }
+	for(MailTemplate templ : MailTemplate.values()){
+	    if( templ.getName().equals(name))
+		return templ;
+	}
+	return null;
+    }
   }
   
   public void delegate
