724ea388bdefe9cc83af21fdca847734d29f91e9
jelli0t <jelli0t@users.noreply.github.com>
2018-07-19 10:54:56 +0200

    Update UserDaoImpl.java

--- src/main/java/org/sobakaisti/security/dao/UserDaoImpl.java
+++ src/main/java/org/sobakaisti/security/dao/UserDaoImpl.java
@@ -41,4 +41,28 @@ public class UserDaoImpl implements UserDao {
 			count = rowCount.longValue();			
 		return count;
 	}
+	
+	@Override
+	public boolean checkIfUserExists(String username, String email) {		 
+		if(TextUtil.isEmpty(username) & TextUtil.isEmpty(email))
+			return false;
+		
+		String HQL_QUERY = "select 1 from org.sobakaisti.security.model.User u where 1=1"
+			+ (TextUtil.notEmpty(username) ? " and u.username = :username" : "")
+			+ (TextUtil.notEmpty(email) ? " and u.email = :email" : "");
+		try {
+			Session session = sessionFactory.getCurrentSession();
+			Query query = session.createQuery(HQL_QUERY);
+
+			if(TextUtil.notEmpty(username)
+			   query.setParameter("username", username);		
+			if(TextUtil.notEmpty(email)
+			   query.setParameter("email", email);
+
+			Integer result = (Integer) query.uniqueResult();
+			return (result != null && result.intValue() == 1);   
+		} catch(Exception ex) {
+			return false;
+		}
+	}
 }
