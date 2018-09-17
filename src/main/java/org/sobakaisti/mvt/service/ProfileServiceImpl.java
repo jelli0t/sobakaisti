40ca1f282ac37a7fdf365890a0df36af6bb6d007
Nemanja <jelli0t@users.noreply.github.com>
2018-09-17 11:20:38 +0200

    Update ProfileServiceImpl.java

--- src/main/java/org/sobakaisti/mvt/service/ProfileServiceImpl.java
+++ src/main/java/org/sobakaisti/mvt/service/ProfileServiceImpl.java
@@ -1,11 +1,16 @@
 package org.sobakaisti.mvt.service;
 
 @Service
-public abstract class ProfileServiceImpl<T extends Profile> {
+public abstract class ProfileServiceImpl<T extends Profile> implements ProfileService<T> {
 
+  private static final Logger logger = LoggerFactory.getLogger(ProfileServiceImpl.class);
+  
   @Autowired
   private ProfileDao<T> postDao;
   
-  
+  @Override
+  boolean removeSocialNetwork(int snid) {
+    return postDao.removeSocialNetwork(int snid);
+  }
   
 }
