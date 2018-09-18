4449bd3689250a613f94a9083f00308e4a6e92d2
Nemanja <jelli0t@users.noreply.github.com>
2018-09-18 13:38:41 +0200

    Update ProfileService.java

--- src/main/java/org/sobakaisti/mvt/service/ProfileService.java
+++ src/main/java/org/sobakaisti/mvt/service/ProfileService.java
@@ -12,4 +12,6 @@ public interface ProfileService<T extends Profile> {
   * Uklanja podatke o profilu na soc.mrezi, samo na osnovu ID-a.
   **/
   boolean removeSocialNetwork(int snid);
+  
+  public T saveOrUpdateProfile(T t);
 }
