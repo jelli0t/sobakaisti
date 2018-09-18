c046affa35d4b0f24fa270626f759de7a8cb5fbe
Nemanja <jelli0t@users.noreply.github.com>
2018-09-18 14:02:32 +0200

    Update ProfileDao.java

--- src/main/java/org/sobakaisti/mvt/dao/ProfileDao.java
+++ src/main/java/org/sobakaisti/mvt/dao/ProfileDao.java
@@ -12,20 +12,20 @@ public interface ProfileDao<T extends Profile> {
 	 * Pronalazi profil na osnovu prosledjenog ID-a
 	 **/
 	T findProfile(int id);
-
 	/**
 	 * Uklanja SocilaNetwork na samo na osnovu ID mreze.
 	 **/
 	boolean removeSocialNetwork(int snid);
-
 	/**
 	 * Uklanja SocilaNetwork na na osnovu ID mreze za profil sa ID.
 	 **/
 	boolean removeProfilesSocialNetwork(int snid, int profileId);
-
 	/**
 	 * Uklanja sve Drustvene mreze za profil sa ID.
 	 **/
-	boolean removeAllProfilesSocialNetworks(int profile);
-
+	boolean removeAllProfilesSocialNetworks(int profile);	
+	/**
+	* Cuva novu ili azurira postojeci profil.
+	**/
+	public T saveOrUpdateProfile(T t);
 }
